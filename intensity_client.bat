5c7a31ad0e38a1658569bf47c77373ed2feba21c
quaker66 <quaker66@gmail.com>
2010-07-27 01:12:38 +0200

    Fix CMake buildsystem for windows. Add vcpp/cmake-built windows binaries and libraries. Mingw build is WIP for now, vcpp works perfectly (built using Visual C++ Express 2008).

--- intensity_client.bat
+++ intensity_client.bat
@@ -1,5 +1,5 @@
 SET OLD_PATH=%PATH%
-SET PATH=windows\dll;Python26;C:\PYTHON26\;src\windows\sdl\lib;src\windows\sdl_image\lib;src\windows\sdl_mixer\lib;%PATH%
+SET PATH=windows\dll;Python26;C:\PYTHON26\;src\windows\sdl_vcpp\lib;src\windows\sdl_image\lib;src\windows\sdl_mixer\lib;%PATH%
 
 SET OLD_PYTHONHOME=%PYTHONHOME%
 SET PYTHONHOME=Python26\lib;Python26\DLLs;C:\Python26\lib;C:\Python26\DLLs;%PYTHONHOME%
@@ -7,25 +7,42 @@ SET PYTHONHOME=Python26\lib;Python26\DLLs;C:\Python26\lib;C:\Python26\DLLs;%PYTH
 SET OLD_PYTHONPATH=%PYTHONPATH%
 SET PYTHONPATH=Python26\lib;Python26\DLLs;C:\Python26\lib;C:\Python26\DLLs;%PYTHONPATH%
 
-SET PREFIX="cbuild\src\client"
-if exist %PREFIX%\CC_Client_Windows-%PROCESSOR_ARCHITECTURE%.exe
-(
-    %PREFIX%\CC_Client_Windows-%PROCESSOR_ARCHITECTURE%.exe %* -r > out_client 2>&1
+if %PROCESSOR_ARCHITECTURE%==AMD64 (
+    if exist bin\CC_Client_Windows-AMD64.exe (
+        SET CCARCH=AMD64
+        goto :run
+    ) else (
+        if exist bin\CC_Client_Windows-x86.exe (
+            SET CCARCH=x86
+            goto :run
+        ) else (
+            echo "No client executable found for %PROCESSOR_ARCHITECTURE%."
+            echo "You can either compile it yourself (and if possible, send to developers)"
+            echo "or wait until we get official binaries for %PROCESSOR_ARCHITECTURE%."
+            goto :finish
+        )
+    )
+) else (
+    if exist bin\CC_Client_Windows-x86.exe (
+        SET CCARCH=x86
+        goto :run
+    ) else (
+        echo "No client executable found for %PROCESSOR_ARCHITECTURE%."
+        echo "You can either compile it yourself (and if possible, send to developers)"
+        echo "or wait until we get official binaries for %PROCESSOR_ARCHITECTURE%."
+        goto :finish
+    )
 )
-else
-(
-    echo "No executable exists for architecture %ccarch%."
-    SET PATH=%OLD_PATH%
-    SET PYTHONHOME=%OLD_PYTHONHOME%
-    SET PYTHONPATH=%OLD_PYTHONPATH%
-    pause
-    goto :end
-)
+
+:run
+bin\CC_Client_Windows-x86.exe %* -r > out_client 2>&1
 
 echo "(If a problem occurred, look in out_client)"
 
+:finish
 pause
 
 SET PATH=%OLD_PATH%
 SET PYTHONHOME=%OLD_PYTHONHOME%
 SET PYTHONPATH=%OLD_PYTHONPATH%
+
