ced6a65644caa58577f5717caf5a1104168c81dd
William Chambers <william@bioselement.com>
2010-09-07 15:28:29 -0400

    Adding emptymap to packages.

---
+++ packages/base/emptymap/map.js
@@ -0,0 +1,44 @@
+// Copyright YEAR YOURNAME
+
+// Textures
+
+Library.include('yo_frankie/');
+
+// Map settings
+
+Map.fogColor(0, 0, 0);
+Map.fog(9999);
+Map.loadSky("skyboxes/philo/sky3");
+Map.skylight(100, 100, 100);
+Map.ambient(20);
+Map.shadowmapAmbient("0x505050");
+Map.shadowmapAngle(300);
+
+//// Player class
+
+registerEntityClass(Player.extend({
+    _class: "GamePlayer",
+}));
+
+//// Application
+
+ApplicationManager.setApplicationClass(Application.extend({
+    _class: "GameApplication",
+
+    getPcClass: function() {
+        return "GamePlayer";
+    },
+
+    // Replace this with appropriate behaviour for when a player falls of the map
+    clientOnEntityOffMap: function(entity) {
+        entity.position = [600,600,600];
+    }
+}));
+
+//// Load permanent entities
+
+if (Global.SERVER) { // Run this only on the server - not the clients
+    var entities = CAPI.readFile("./entities.json");
+    loadEntities(entities);
+}
+
